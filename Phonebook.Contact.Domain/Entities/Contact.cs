64cb8c0cf7d0a2e00a0732275505a4eab16a2b9c
ozyptic <ahmetozancinar@gmail.com>
2023-11-05 15:52:25 +0300

    added contact entities and enums

---
+++ Phonebook.Contact.Domain/Entities/Contact.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook.Contact.Domain.Entities
+{
+    public class Contact
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public required string Id { get; set; }
+        public required string Name { get; set; }
+        public required string LastName { get; set; }
+        public required string Company { get; set; }
+    }
+}
---
+++ Phonebook.Contact.Domain/Enums/ContactInfoType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook.Contact.Domain.Enums
+{
+    public enum ContactInfoType
+    {
+        Phone = 0,
+        Email = 1,
+        Location = 2
+    }
+}
